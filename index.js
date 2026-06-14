935e2f96a5ea4e416ef0c8b63a10c3fc1f0694b0
Ahsanul Azim Plabon <ahsanulazim6@gmail.com>
2026-06-14 11:07:52 +0600

    2nd commit

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to Pretypet Backend" });
+  res.json("Welcome to Pretypet Backend");
 });
 
 app.listen(PORT, () => {
